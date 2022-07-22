ee247c6c119663c013a638560a6c08d814037c17
santdev404 <santdev404@gmail.com>
2022-07-22 13:04:31 -0500

    webpack

---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre) =>{
+    console.log('creando etiqueta');
+    
+    const h1 = document.createElement('h1');
+    h1.innerText= `Hello ${nombre}`;
+    document.body.append(h1);
+
+    //Img
+    const img = document.createElement('img');
+    img.src = webpacklogo;
+    document.body.append(img);
+
+}
